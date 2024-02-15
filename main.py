f424b0a010bc93e38f7cfd8120efe75459dec1f1
sunriser <sunriser098@gmail.com>
2024-02-15 14:37:48 -0600

    Create an initial PDF output for each excel file.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import glob
 import os
+from fpdf import FPDF
+from pathlib import Path
 
 # use glob to read all .xlsx files
 # path = os.getcwd()
@@ -13,5 +15,11 @@ print(excel_files)
 # loop over .xlsx files for data
 for file in excel_files:
     df = pd.read_excel(file, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(file).stem
+    invoice_nbr = filename.split("-")[0]
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nbr}")
+    pdf.output(f"output_pdfs/{filename}.pdf")
 
